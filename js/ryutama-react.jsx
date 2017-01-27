1242115d7eb0cf5bda1362e9ad1c71b2d027ced1
oldgeese <hiro.onoda@gmail.com>
2017-01-27 23:05:13 +0900

    fixed to use componentDidMount

--- js/ryutama-react.jsx
+++ js/ryutama-react.jsx
@@ -4,27 +4,29 @@ import request from 'superagent';
 
 const url = "http://charasheet.vampire-blood.net/962930.json";
 
-var data = {};
-
-request.get(url).use(jsonp)
-  .then(
-    (obj) => {
-      data = obj;
-    }
-  )
-  .catch(
-    (err)=> {console.error(err);}
-  );
-  
 class RyutamaSheet extends React.Component {
+  componentDidMount() {
+    request.get(this.props.url).use(jsonp)
+      .then(
+        (res) => {
+          this.setProps({data: res.body});
+        }
+      )
+      .catch(
+        (err)=> {console.error(err);}
+      );
+  }
   render() {
-    return (
-      <div>{this.props.data}</div>
-    );
+    const d = this.props.data;
+    if (d) {
+      return (
+        <div>{d.pc_name}</div>
+      );
+    }
   }
 }
 
 ReactDOM.render(
-  <RyutamaSheet data={data}/>,
+  <RyutamaSheet url={url}/>,
   document.getElementById('root')
 );
