bffa13016e1403a6db6c049cec780c625072018f
oldgeese <hiro.onoda@gmail.com>
2017-01-27 22:29:37 +0900

    superagent を使って実装を始めてみた

--- js/ryutama-react.jsx
+++ js/ryutama-react.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import request from 'superagent';
+
+const url = "http://charasheet.vampire-blood.net/962930.json";
+
+var data = {};
+
+request.get(url).use(jsonp)
+  .then(
+    (obj) => {
+      data = obj;
+    }
+  )
+  .catch(
+    (err)=> {console.error(err);}
+  );
+  
+class RyutamaSheet extends React.Component {
+  render() {
+    return (
+      <div>{this.props.data}</div>
+    );
+  }
+}
 
 ReactDOM.render(
-  <h1>Hello, world!!!!!!</h1>,
+  <RyutamaSheet data={data}/>,
   document.getElementById('root')
 );
