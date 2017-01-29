ae3c664e67c84c08e118469645da4d249142c2cf
oldgeese <hiro.onoda@gmail.com>
2017-01-29 20:07:23 +0900

    renamed jsx file

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-  entry:"./js/ryutama-react.jsx",
+  entry:"./js/app.jsx",
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
