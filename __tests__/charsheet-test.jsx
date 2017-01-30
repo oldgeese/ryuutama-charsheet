06eab24580d81fc96ec2fa901a11d7c1565d006a
oldgeese <hiro@nestofgeese.local>
2017-01-31 01:11:21 +0900

    console.logを使うところをコメントアウト

--- __tests__/charsheet-test.jsx
+++ __tests__/charsheet-test.jsx
@@ -9,14 +9,14 @@ import getSheetData from '../js/sheetdata.jsx';
 describe('<CharSheet/>', () => {
   it('full DOM rendering', () => {
     const wrapper = mount(<CharSheet />);
-    let tree = wrapper.debug();
-    console.log(tree);
+    // let tree = wrapper.debug();
+    // console.log(tree);
     expect(wrapper.find('CharSheet')).toHaveLength(1);
 
     return getSheetData()
       .then((res) => {
         wrapper.update();
-        console.log(wrapper.debug());
+        // console.log(wrapper.debug());
         expect(wrapper.find('CharSheet')).toHaveLength(1);
         expect(wrapper.find('RyutamaSheet')).toHaveLength(1);
       });
