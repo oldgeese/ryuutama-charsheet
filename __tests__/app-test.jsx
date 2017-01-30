d31f3d9ad4039b73b3283162ef1369493007bf9f
oldgeese <hiro@nestofgeese.local>
2017-01-31 00:52:39 +0900

    テスト追加、修正

--- __tests__/app-test.jsx
+++ __tests__/app-test.jsx
@@ -1,13 +1,5 @@
-jest.mock('../js/sheetdata');
 
-import React from 'react';
-import { shallow, mount, render } from 'enzyme';
-
-import CharSheet from '../js/app';
-
-
-describe('<CharSheet/>' ()=> {
-  it('full DOM rendering', () => {
-    const wrapper = mount(<CharSheet />);
+describe('app.jsx', () => {
+  it('no need to test', () => {
   });
 });
---
+++ __tests__/charsheet-test.jsx
@@ -0,0 +1,24 @@
+jest.mock('../js/sheetdata.jsx');
+
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+
+import CharSheet from '../js/charsheet.jsx';
+import getSheetData from '../js/sheetdata.jsx';
+
+describe('<CharSheet/>', () => {
+  it('full DOM rendering', () => {
+    const wrapper = mount(<CharSheet />);
+    let tree = wrapper.debug();
+    console.log(tree);
+    expect(wrapper.find('CharSheet')).toHaveLength(1);
+
+    return getSheetData()
+      .then((res) => {
+        wrapper.update();
+        console.log(wrapper.debug());
+        expect(wrapper.find('CharSheet')).toHaveLength(1);
+        expect(wrapper.find('RyutamaSheet')).toHaveLength(1);
+      });
+  });
+});
