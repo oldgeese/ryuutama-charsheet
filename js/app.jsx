26090bb64f68833fa46ffce5e5495e0317528179
oldgeese <hiro@nestofgeese.local>
2017-02-04 01:56:17 +0900

    キャラクターID（MD５はまだ）入力機能実装

--- js/app.jsx
+++ js/app.jsx
@@ -6,27 +6,49 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      charIdInput = false,
-      charId = ""
+      charIdInput: false,
+      charId: "962930"
     };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleInputChange(event) {
+    const name = event.target.name;
+    this.setState({
+      [name]: event.target.value,
+    });
+  }
+  handleSubmit(event) {
+    if (this.state.charId) {
+      this.setState({
+        charIdInput: true,
+      })
+    }
+    event.preventDefault();
   }
   render() {
     if (this.state.charIdInput) {
-      <CharSheet charId ={this.state.charId} />
+      return (
+        <CharSheet charId ={this.state.charId} />
+      );
     } else {
-      <div>
-        <form>
+      return (
         <div>
-          キャラクターのIDを入力してください。
+          <form onSubmit={this.handleSubmit}>
+            <div>
+              キャラクターのIDを入力してください。
+            </div>
+            <div>
+              <input type="text" size="60" name="charId"
+                value={this.state.charId} onChange={this.handleInputChange} />
+            </div>
+            <div>
+              <input type="submit" value="入力" />
+            </div>
+          </form>
         </div>
-        <div>
-          <input type="text" size="60" name="charId"/>
-        </div>
-        <div>
-          <button onClick={()=> this.handleInputClick()}>入力</button>
-        </div>
-        </form>
-      </div>
+      );
     }
   }
 }
