794299efd15d4bba8e490205325f87944e8989a5
oldgeese <hiro@nestofgeese.local>
2017-02-04 20:57:39 +0900

    separate app.jsx into app.jsx and index.jsx. and added test for app.jsx

--- __tests__/app-test.jsx
+++ __tests__/app-test.jsx
@@ -1,5 +1,21 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+
+import App from '../js/app.jsx';
 
 describe('app.jsx', () => {
-  it('no need to test', () => {
+  it('should render <div /> when charId not yet inputed', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('form')).toHaveLength(1);
   });
+
+  it('should render <CharSheet /> when charId inputed', () => {
+    const wrapper = shallow(<App />);
+    wrapper.setState({
+      charId: "962930",
+      charIdInput: true,
+    });
+    expect(wrapper.find('CharSheet')).toHaveLength(1);
+  });
+
 });
