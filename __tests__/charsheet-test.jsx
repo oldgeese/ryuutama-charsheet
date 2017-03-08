9471c2580074859dbabd4bd8d6598e0facc5e001
oldgeese <hiro.onoda@gmail.com>
2017-03-08 21:27:16 +0900

    fixed charsheet-test

--- __tests__/charsheet-test.jsx
+++ __tests__/charsheet-test.jsx
@@ -13,12 +13,12 @@ describe('charsheet.jsx', () => {
     // console.log(tree);
     expect(wrapper.find('CharSheet')).toHaveLength(1);
 
-    // return sheetData("962930")
-    //   .then((res) => {
-    //     wrapper.update();
-    //     // console.log(wrapper.debug());
-    //     expect(wrapper.find('CharSheet')).toHaveLength(1);
-    //     expect(wrapper.find('RyutamaSheet')).toHaveLength(1);
-    //   });
+    return sheetData("962930")
+      .then((res) => {
+        wrapper.update();
+        // console.log(wrapper.debug());
+        expect(wrapper.find('CharSheet')).toHaveLength(1);
+        expect(wrapper.find('RyutamaSheet')).toHaveLength(1);
+      });
   });
 });
