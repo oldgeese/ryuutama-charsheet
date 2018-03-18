5300ddbd89a0df840a543f12d3c8b0ecd62f392f
oldgeese <hiro.onoda@gmail.com>
2018-03-19 01:12:08 +0900

    updated test to fit newest sources

--- js/app.jsx
+++ js/app.jsx
@@ -1,11 +1,22 @@
 // @flow
 import React from 'react';
+import Input from './input.jsx';
+import CharSheet from './charsheet.jsx';
+import Search from './search.jsx';
+import { Route, Redirect, Switch, withRouter} from 'react-router-dom'
+
+const InputWithRouter = withRouter(Input);
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        {this.props.children}
+        <Switch>
+          <Route path="/input" component={InputWithRouter} />
+          <Route path="/char/:charId" component={CharSheet} />
+          <Route path="/search/:mode/:searchString" component={Search} />
+          <Redirect from="*" to="/input" />
+        </Switch>
       </div>
     );
   }
--- js/index.jsx
+++ js/index.jsx
@@ -2,24 +2,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app.jsx';
-import Input from './input.jsx';
-import CharSheet from './charsheet.jsx';
-import Search from './search.jsx';
-import { BrowserRouter as Router, Route, Redirect, Switch, withRouter} from 'react-router-dom'
-
-const InputWithRouter = withRouter(Input);
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
-  <Router>
-    <App>
-      <Switch>
-        <Route path="/input" component={InputWithRouter} />
-        <Route path="/char/:charId" component={CharSheet} />
-        <Route path="/search/:mode/:searchString" component={Search} />
-        <Redirect from="*"to="/input" />
-      </Switch>
-    </App>
-  </Router>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
   ,
   document.getElementById('root')
 );
