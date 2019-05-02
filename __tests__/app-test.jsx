0df6f300b705ce22e18c845e08904f07bba870c3
oldgeese <hiro.onoda@gmail.com>
2019-05-02 19:30:12 +0900

    update tests

--- __tests__/app-test.jsx
+++ __tests__/app-test.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import App from '../js/app.jsx';
 import { MemoryRouter } from 'react-router-dom'
+import { createStore } from 'redux';
+import searchApp from '../js/reducers';
+import { Provider } from 'react-redux';
+
+const store = createStore(searchApp);
 
 test('should render <div />', () => {
   const wrapper = shallow(<App />);
@@ -10,9 +15,11 @@ test('should render <div />', () => {
 
 test('should have Input component when "/input"', () => {
   const app = mount(
-    <MemoryRouter initialEntries={["/input"]}>
+    <Provider store={store}>
+      <MemoryRouter initialEntries={["/input"]}>
       <App />
-    </MemoryRouter>
+      </MemoryRouter>
+    </Provider>
   );
 
   expect(app.find("Input")).toHaveLength(1);
@@ -20,9 +27,11 @@ test('should have Input component when "/input"', () => {
 
 test('should have CharSheet component when "/char/:charId"', () => {
   const app = mount(
-    <MemoryRouter initialEntries={["/char/999999"]}>
-      <App />
-    </MemoryRouter>
+    <Provider store={store}>
+      <MemoryRouter initialEntries={["/char/999999"]}>
+        <App />
+      </MemoryRouter>
+    </Provider>
   );
 
   expect(app.find("CharSheet")).toHaveLength(1);
@@ -30,9 +39,11 @@ test('should have CharSheet component when "/char/:charId"', () => {
 
 test('should have Search component when "/search/tag/abc"', () => {
   const app = mount(
-    <MemoryRouter initialEntries={["/search/tag/abc"]}>
-      <App />
-    </MemoryRouter>
+    <Provider store={store}>
+      <MemoryRouter initialEntries={["/search/tag/abc"]}>
+        <App />
+      </MemoryRouter>
+    </Provider>
   );
 
   expect(app.find("Search")).toHaveLength(1);
@@ -40,9 +51,11 @@ test('should have Search component when "/search/tag/abc"', () => {
 
 test('should have Search component when "/search/title/abc"', () => {
   const app = mount(
-    <MemoryRouter initialEntries={["/search/title/abc"]}>
-      <App />
-    </MemoryRouter>
+    <Provider store={store}>
+      <MemoryRouter initialEntries={["/search/title/abc"]}>
+        <App />
+      </MemoryRouter>
+    </Provider>
   );
 
   expect(app.find("Search")).toHaveLength(1);
@@ -50,9 +63,11 @@ test('should have Search component when "/search/title/abc"', () => {
 
 test('should have Search component when "/search/name/abc"', () => {
   const app = mount(
-    <MemoryRouter initialEntries={["/search/name/abc"]}>
-      <App />
-    </MemoryRouter>
+    <Provider store={store}>
+      <MemoryRouter initialEntries={["/search/name/abc"]}>
+        <App />
+      </MemoryRouter>
+    </Provider>
   );
 
   expect(app.find("Search")).toHaveLength(1);
@@ -60,9 +75,11 @@ test('should have Search component when "/search/name/abc"', () => {
 
 test('should have Input component when "/xyz"', () => {
   const app = mount(
-    <MemoryRouter initialEntries={["/xyz"]}>
-      <App />
-    </MemoryRouter>
+    <Provider store={store}>
+      <MemoryRouter initialEntries={["/xyz"]}>
+        <App />
+      </MemoryRouter>
+    </Provider>
   );
 
   expect(app.find("Input")).toHaveLength(1);
